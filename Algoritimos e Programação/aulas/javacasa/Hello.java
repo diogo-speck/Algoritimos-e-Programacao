545dac9fce753ef5533d2eeb5d98116333c866d0
diogo-speck <147861369+diogo-speck@users.noreply.github.com>
2026-03-25 00:34:22 +0000

    1

--- Algoritimos e Programação/aulas/javacasa/Hello.java
+++ Algoritimos e Programação/aulas/javacasa/Hello.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 public class Hello{
 public static void main (String[]args){
 Scanner sc = new Scanner(System.in); 
+while (true){
     System.out.println("Treinando Java, deseja treinar também? ");
     String confirmacao = sc.nextLine();
     confirmacao = confirmacao.toLowerCase();
     // ex. SIM -> sim
     if (confirmacao.equalsIgnoreCase("sim")||(confirmacao.equals("ss"))||(confirmacao.equals("s"))||(confirmacao.equals("zim"))){
         System.out.println("Certo, aqui vai uma lista dos principais comandos: ");
+        System.out.println("System.out.printf('Nome: %s, Idade: %d%n', 'Diogo', 18");
+        System.out.println("Switch (variavel){case x:;}");
+        System.out.println("for (tipo) i = valor; i (intervalo); i++(mais 1 toda vez do laço){executa uma ação em realação a i}");
+        
     }
     else{
         System.out.println("Ok, sem problemas");
+        sc.close();
     }
-    
-sc.close();
+}    
 }
 }

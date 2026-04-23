dfa8005be6817e8dd7b1305cf273cd694fc1fa40
diogo-speck <diogospeck@gmail.com>
2026-04-23 16:43:45 -0300

    terminei as atividades que eram tarefa do dia 22/04/2026 utilizando for e outras funcionalidades de arraylist

--- aulas/aula07/m2AtividadeEmSalaTarefa.java
+++ aulas/aula07/m2AtividadeEmSalaTarefa.java
@@ -8,7 +8,7 @@ public class m2AtividadeEmSalaTarefa {
     System.out.println("Escolha a atividade:");
     System.out.println("1 - Leitura de números inteiros com múltiplas condições de paradas");
     System.out.println("2 - Pesquisa de satisfação");
-    System.out.println("3 - ");
+    System.out.println("3 - Menu de Cadastro");
     System.out.printf("Escolha: ");
     int escolha = sc.nextInt();
         switch (escolha) {
@@ -30,7 +30,7 @@ public class m2AtividadeEmSalaTarefa {
                 
             } while (!(contagem>=10 && soma>100));
             media = soma/contagem;
-            System.out.println("A soma final foi "+soma+" e a quantidade de elementos válidos informados foi "+lista.size()+". Logo a média desses valores foi "+media);
+            System.out.println("A soma final foi "+soma+" e a quantidade de elementos válidos informados foi "+lista.size()+". Logo a média desses valores foi aproximadamente "+(String.format("%.2f",media)));
             System.out.println("Os elementos, em ordem, foram: "+lista);
             break;            
             
@@ -40,7 +40,7 @@ public class m2AtividadeEmSalaTarefa {
                 double mediaa;
                 ArrayList<Integer> vezes = new ArrayList<>();
                 System.out.println("Deseja nos avaliar? (s/n) ");
-                avaliar = sc.next();
+                avaliar = sc.next().toLowerCase();
                 if (avaliar.equals("s")){
                     System.out.printf("Quantos clientes vão nos avaliar? ");
                     clientes = sc.nextInt();
@@ -55,24 +55,20 @@ public class m2AtividadeEmSalaTarefa {
                             System.out.println("Escolha uma nota entre 1 e 5 cliente "+i);
                             nota = sc.nextInt();
                             if (nota>=1 && nota<=5){
-                                notas = notas+nota;
                                 contador+=1;
                                 vezes.add(nota);
+                                notas = notas+nota;
                             }
                             else{
                                 while (!(nota>=1 && nota<=5)) { 
                                     System.out.println("Por favor, coloque a nota dentro do intervalo de 1 e 5 cliente "+i);
                                     nota = sc.nextInt();
                                 }
-                                notas = notas+nota;
-                                vezes.add(nota);
                             }
-                        
                         }
                         mediaa = Math.round((double) notas / contador);
-                        contador+=1;
-                        System.out.println("A quantidade de respostas registradas foram "+contador);
-                        System.out.println("A média da nossa avaliação foi aproximadamente "+mediaa);
+                        System.out.println("Foram registradas "+vezes.size()+" respostas válidas");
+                        System.out.println("A média da nossa avaliação foi de aproximadamente "+mediaa);
                         
                         for (int j=1; j<=5;j++){
                             int conferir=0;
@@ -97,7 +93,88 @@ public class m2AtividadeEmSalaTarefa {
                 else{
                     System.out.println("Ok, perguntamos outra hora então.");
                 }
+            break;            
+            
+            case 3:
+                int opcao;
+                String cadastro, consulta;
+                ArrayList<String> coisas = new ArrayList<>();
+                do { 
+                    System.out.println("--------Menu--------");
+                    System.out.println("1 - CADASTRAR");
+                    System.out.println("2 - CONSULTAR");
+                    System.out.println("3 - ATUALIZAR/LIMPAR");
+                    System.out.println("4 - EXCLUIR/DELETAR");
+                    System.out.println("5 - SAIR");
+                    System.out.printf("Escolha: ");
+                    opcao = sc.nextInt();
+                    
+                    switch (opcao) {
+                        case 1:
+                            System.out.println("Você tem "+coisas.size()+" coisa(s) cadastradas, sendo ela(s):");
+                            System.out.println(coisas);
+                            System.out.println("Digite algo para cadastrar");
+                            cadastro = sc.next();
+                            if (coisas.contains(cadastro)){
+                                System.out.println("'"+cadastro+"' já está cadastrado");
+                            }
+                            else{
+                            coisas.add(cadastro);
+                            System.out.println("Cadastro '"+cadastro+"' efetuado com sucesso");
+                            System.out.println(coisas);
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Você tem "+coisas.size()+" coisa(s) cadastradas, sendo ela(s):");
+                            System.out.println(coisas);
+                            System.out.println("Digite o que deseja consultar: ");
+                            consulta = sc.next();
+                            if (coisas.contains(consulta)){
+                                System.out.println("'"+consulta+"' está cadastrado");
+                            }
+                            else{
+                                System.out.println("'"+consulta+"' não está cadastrado");
+                            }
+                            System.out.println("Consulta realizada");
+                            break;
+                        case 3:
+                            System.out.println("Você tem "+coisas.size()+" coisa(s) cadastradas, sendo ela(s):");
+                            System.out.println(coisas);
+                            System.out.println("Deseja atualizar/limpar seu cadastro? (s/n): ");
+                            consulta = sc.next().toLowerCase();
+                            if (consulta.equals("s")){
+                                coisas.clear();
+                                System.out.println("Ok, cadastro atualizado/limpo");
+                                System.out.println(coisas);
+                            }
+                            break;
+                        case 4:
+                            System.out.println("Você tem "+coisas.size()+" coisa(s) cadastradas, sendo ela(s):");
+                            System.out.println(coisas);
+                            System.out.println("Digite o que deseja excluir/deletar: ");
+                            consulta = sc.next();
+                            if (coisas.contains(consulta)){
+                                coisas.remove(consulta);
+                                System.out.println("Exclusão realizada");
+                                System.out.println("'"+consulta+"' foi removido");
+                            }
+                            else{
+                                System.out.println("'"+consulta+"' não está cadastrado");
+                            }
+                            System.out.println("Seu cadastro agora está assim:");
+                            System.out.println(coisas);
 
+                            break;
+                        default:
+                            if (opcao!=5){
+                            System.out.println("Opção inválida! Tente novamente");
+                            }
+                            break;
+                    }
+
+
+                } while (opcao!=5);
+                System.out.println("Finalizando o programa...");
             default: 
                 sc.close();
         }

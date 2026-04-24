a92e81d78666b6299d5604928b4d636c589357bb
diogo-speck <diogospeck@gmail.com>
2026-04-23 23:51:21 -0300

    tarefa para o dia 28/04/2026 terminada, implementação dos exercícios usando apenas conteúdos que vimos

--- aulas/aula07/atividadeEmSala02M2.java
+++ aulas/aula07/atividadeEmSala02M2.java
@@ -51,16 +51,86 @@ public class atividadeEmSala02M2 {
                     System.out.println("O fatorial do número "+numero+" é "+fatorial);
                     break;
                 case 4:
-
+                    int termos, f=0, g=1, h;
+                    System.out.printf("Insira quantos termos você quer que a sequência tenha: ");
+                    termos = sc.nextInt();
+                    if (termos <= 0){
+                        System.out.println("Digite um número inteiro maior que zero");
+                        break;
+                    }
+                    else if (termos == 1){
+                        System.out.println(f);
+                    }
+                    else{
+                        System.out.println(f);
+                        System.out.println(g);
+                        if (termos > 2){
+                            for (int d=0; d<=termos-2; d++){
+                                h=f+g;
+                                System.out.println(h);
+                                f=g;
+                                g=h;
+                            }
+                        }
+                    }
                     break;
                 case 5:
-
+                    int tio;
+                    System.out.printf("Insira o limite máximo do intervalo: ");
+                    tio = sc.nextInt();
+                    System.out.println("Os números que são primos entre o intervalo de 2 e "+tio+" são:");
+                    for(int k=2; k<=tio; k++){
+                        boolean primo = true;
+                        for(int l=2; l<=Math.sqrt(k); l++){
+                            if (k%l==0){
+                                primo = false; 
+                            }
+                        }
+                        if (primo==true){
+                            System.out.printf(k+" ");
+                        }
+                    }
                     break;
                 case 6:
-
+                    int imperfeito, m=2;
+                    double perfeitos=0;
+                    System.out.printf("Insira um número inteiro maior que zero: ");
+                    imperfeito = sc.nextInt();
+                    for (m=2;m>=2;m++){
+                        perfeitos = Math.pow(2, m-1)*(Math.pow(2, m)-1);
+                        //System.out.println(perfeitos);
+                        if (perfeitos>=imperfeito){
+                            break;
+                        }
+                    }
+                    double imperfeitoDouble = (double) imperfeito;
+                    if (perfeitos==imperfeitoDouble){
+                        System.out.println(imperfeito+" é um número perfeito");
+                    }
+                    else{
+                        System.out.println(imperfeito+" não é um número perfeito");
+                    }
                     break;
                 case 7:
+                    System.out.printf("Insira um número inteiro positivo e veja a mágica acontecer: ");
+                    int padrao = sc.nextInt();
+                    
+                    /* Padrão errado kkkkk
 
+                    for(int n=1; n<=padrao; n++){
+                        for(int o=1; o<=n; o++){
+                            System.out.printf(n+" ");
+                        }
+                        System.out.println();
+                    }
+                    */
+
+                    for(int n = 1; n <= padrao; n++) {
+                        for(int o = 1; o <= n; o++) {      
+                            System.out.printf(o + " ");    
+                        }
+                        System.out.println();          
+                    }
                     break;
 
                 default:

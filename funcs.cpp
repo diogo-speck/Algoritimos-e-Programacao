b140aca371291cffccc328ced9371ca89e1b5f13
diogo-speck <diogospeck@gmail.com>
2026-06-25 10:33:25 -0300

    criando o arquivo para fazer a Atividade de Pesquisa: Funções em C++

---
+++ funcs.cpp
@@ -0,0 +1,16 @@
+/*
+5. Desenvolva e explique um exemplo para cada uma das situações abaixo: 
+• Uma função que exiba uma mensagem na tela; 
+• Uma função que receba dois números e apresente a soma; 
+• Uma função que receba dois números e retorne o maior valor. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
--- src/resenha_do_matheus/matrizPerdida.java
+++ src/resenha_do_matheus/matrizPerdida.java
@@ -71,10 +71,6 @@ public static void main(String[] args) {
             }
     }
     System.out.println("\nTiveram "+contador+" números "+num);
-    
-    
-
-   
 
     sc.close();
 }

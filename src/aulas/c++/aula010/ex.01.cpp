aaa3ee4607b5c4853b784c70fa798d705a7ff5dd
diogo-speck <diogospeck@gmail.com>
2026-08-03 21:46:03 -0300

    ex.1 da aula 010 feito como ex.2 com switch case

---
+++ src/aulas/c++/aula010/ex.01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    float medida = 0;
+    char tipo = ' ';
+
+    cout << "Conversão de unidades com if-else" << endl;
+    cout << "Digite uma medida no padrão centímetros ou polegadas (ex. 25.7 c / 13.9 p):" << endl;
+    cin >> medida >> tipo;
+    switch (tipo)
+    {
+    case 'c':
+        medida = medida/2.54;
+        cout << "Sua unidade de medida convertida de centímetros para polegadas é " << medida << "\"" << endl;
+        break;
+
+    case 'p':
+        medida = medida*2.54;
+        cout << "Sua unidade de medida convertida de polegadas para centímetros é " << medida << "cm" << endl;
+        break;
+    
+    default:
+        cout << "Unidade inválida";
+    }
+
+    // 1 polegada = 2,54 centímetros
+
+    
+    return 0;
+}

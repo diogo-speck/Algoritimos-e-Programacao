208d170d045d206d7a099fbf3658c706ebc8cacb
Diogo Speck <diogospeck@gmail.com>
2026-08-05 17:39:36 -0300

    exercícios da aula010 terminados

--- src/aulas/c++/aula010/ex07.cpp
+++ src/aulas/c++/aula010/ex07.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+//#include <limits>
 using namespace std;
 
 int main() {
@@ -33,7 +35,38 @@ int main() {
     }
     cout << "\n1.000.000.000 grãos - " << c << "\n" << endl;
 
-    // Todo Item 2 — Representação do tabuleiro
+    // Item 2 — Representação do tabuleiro
+
+    double tabuleiro [8][8]; // Tem que ser double para a Notação Científica funcionar
+    graos = 1;
+    c = 1;
+    int i=0,j=0;
+
+    for (graos; c<65; graos = graos*2){
+        //cout << graos << " grãos | " << c << " | (" << 2*graos-1 << ")" << endl;
+        if (c==64){
+            graos--; // Tirei 1 unidade para não dar Overflow
+        }
+        tabuleiro[i][j] = graos;
+        c++;
+        if ((j+1)%8==0){
+            i = (i+1)%8;
+        }
+        j = (j+1)%8;
+    }
+
+    for (int i=0; i<size(tabuleiro); i++){
+        for (int j=0; j<size(tabuleiro[0]); j++){ // Melhor para matrizes quadradas
+            cout << " | " << scientific << setprecision(3) << tabuleiro[i][j] << " | "; // Notação científica com 4 casas decimais
+        }
+        cout << "\n" << endl;
+    }
+
+    /*cout << numeric_limits<long long int>::max();
+        "Nove quintilhões, duzentos e vinte e três quatrilhões,
+         trezentos e setenta e dois trilhões, trinta e seis bilhões,
+         oitocentos e cinquenta e quatro milhões, setecentos e setenta e cinco mil, oitocentos e sete."
+    */
 
 
     return 0;

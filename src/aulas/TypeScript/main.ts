485b4f76fe17a245c28af98094d6a427c97081aa
diogospeck <diogospeck@gmail.com>
2026-08-19 21:56:07 -0300

    entrada e saída de dados em typescript/javascript + mudando uns arquivos para compatibilidade

--- src/aulas/TypeScript/main.ts
+++ src/aulas/TypeScript/main.ts
@@ -9,8 +9,23 @@ estruturas(if,else,for,for each,while,switch case, etc)
 funções
 */
 
+import readline from "readline"; // Entrada de dados (precisa da biblioteca readline para criar uma interface no terminal)
+
 console.log("Hello World")
 
-if (0/2 == 0) {
+if (0/2 === 0) { // Recomendado usar 3= para comparação de valor e tipo
     console.log(true)
   }
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+terminal.question("Digite seu nome: ", (nome) =>{ // interface no terminal
+  // Recebe uma string na função .question e atribui a uma variável
+  console.log(`Seu nome é ${nome}`) // Saída de dados
+  terminal.close();
+}); // fechando o callback
+
+// executar usando o comando: npx tsx "src\aulas\TypeScript\main.ts"
